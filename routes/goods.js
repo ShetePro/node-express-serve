4c00507c0abd709f73d24a64b3bb57a9a98d1956
ShetePro <sheteprolin@gmail.com>
2024-12-09 17:18:27 +0800

    feat(routes): goods 添加月度统计接口、设置利润表计算

--- routes/goods.js
+++ routes/goods.js
@@ -6,14 +6,15 @@ import {
 } from "../utils/responseUtil.js";
 import { urlPrefix } from "./index.js";
 import { ObjectId } from "mongodb";
+import { getCurrentTimestamp } from "../utils/date.js";
 
 export default function initGoodsRouter(app) {
+  // 新增
   app.post(`${urlPrefix}/goods`, async (req, res) => {
     const body = {
       ...req.body,
       createDate: Date.toString(),
     };
-    const collection = getDatabaseCollection("goods");
     if (!body.goodsName) {
       return res.send({
         code: 400,
@@ -21,11 +22,25 @@ export default function initGoodsRouter(app) {
         msg: "商品名称不能为空",
       });
     }
+    const collection = getDatabaseCollection("goods");
     const result = await collection.insertOne({
       ...body,
       addUser: req.user?.id,
     });
-    return res.send(successResponse(result?._id));
+    const goods = await collection.findOne({
+      _id: result.insertedId,
+    });
+    // 同步添加到goods record中
+    const recordCollection = await getDatabaseCollection("goods_record");
+    recordCollection.insertOne({
+      goodsId: goods._id.toString(),
+      quantity: goods.quantity,
+      totalNum: goods.quantity * goods.price,
+      price: goods.price,
+      createDate: Date.now(),
+      operator: req.user.id,
+    });
+    return res.send(successResponse(goods?._id));
   });
   // 商品列表
   app.get(`${urlPrefix}/goodsList`, async (req, res) => {
@@ -48,6 +63,7 @@ export default function initGoodsRouter(app) {
     });
     return res.send(successResponse(clear_id(result)));
   });
+  // 入库 or 出货
   app.post(`${urlPrefix}/goods/record`, async (req, res) => {
     const body = {
       ...req.body,
@@ -72,7 +88,12 @@ export default function initGoodsRouter(app) {
     const collection = getDatabaseCollection("goods_record");
     const goodsCollection = getDatabaseCollection("goods");
     const num = body.type === 1 ? body.quantity : -body.quantity;
-    const goods = await goodsCollection.updateOne(
+    if (body.type === 2) {
+      await setGoodsProfit(body, collection);
+    } else {
+      body.sellNum = 0;
+    }
+    await goodsCollection.updateOne(
       {
         _id: new ObjectId(body.goodsId),
       },
@@ -83,6 +104,7 @@ export default function initGoodsRouter(app) {
         $inc: { quantity: num },
       },
     );
+
     const result = await collection.insertOne(body);
     return res.send(successResponse(result?._id));
   });
@@ -106,5 +128,99 @@ export default function initGoodsRouter(app) {
     const result = collection.find(query).sort(sort);
     const list = await getMongodbList(result);
     return res.send(successResponse(list));
+  });
+  // 商品月度统计
+  app.post(`${urlPrefix}/goods/month-statistics`, async (req, res) => {
+    const { goodsId } = req.body;
+    if (!goodsId) {
+      return res.send({
+        code: 400,
+        data: null,
+        msg: "商品id不能为空",
+      });
+    }
+    const sort = { createDate: -1 };
+    const [startTime, endTime] = getCurrentTimestamp();
+    console.log(startTime, endTime);
+    const query = {
+      goodsId,
+      // createdDate: {
+      //   $gte: startTime,
+      //   $lte: endTime,
+      // },
+    };
+    const statisticsData = {
+      sellNum: 0,
+      sellPrice: 0,
+      profitPrice: 0,
+    };
+    const collection = getDatabaseCollection("goods_profits");
+    const result = await collection.find(query).sort(sort);
+    for await (const record of result) {
+      console.log(record, "record");
+      statisticsData.sellNum += record.quantity;
+      statisticsData.sellPrice += record.price * record.quantity;
+      statisticsData.profitPrice += record.netProfit;
+    }
+    console.log(statisticsData);
+    return res.send(successResponse(statisticsData));
+  });
+}
+export async function setGoodsProfit(body, collection) {
+  let profit = 0;
+  let num = body.quantity;
+  const profitCollection = getDatabaseCollection("goods_profits");
+  const buyRecords = await collection.find({
+    goodsId: body.goodsId,
+    type: 1,
+    $expr: { $ne: ["$quantity", "$sellNum"] },
+  });
+  const sellData = [];
+  for await (const buy of buyRecords) {
+    const { price, sellNum, quantity } = buy;
+    const over = quantity - sellNum;
+    if (over === 0) break;
+    const isOver = num > over; // 是否全部出售
+    const sellPrice = isOver
+      ? over * (body.price - price)
+      : num * (body.price - price);
+    sellData.push({
+      id: buy._id.toString(),
+      price: buy.price,
+      quantity: num > over ? quantity : num,
+      sellPrice,
+    });
+    // 处理入库的商品
+    if (isOver) {
+      profit += sellPrice;
+      num -= over;
+      collection.updateOne(
+        {
+          _id: buy._id,
+        },
+        {
+          $set: { sellNum: Number(quantity) },
+        },
+      );
+    } else {
+      profit += sellPrice;
+      collection.updateOne(
+        {
+          _id: buy._id,
+        },
+        {
+          $set: { sellNum: Number(sellNum + num) },
+        },
+      );
+      break;
+    }
+  }
+  return await profitCollection.insertOne({
+    goodsId: body.goodsId,
+    netProfit: profit,
+    quantity: body.quantity,
+    price: body.price,
+    sellData,
+    createDate: Date.now(),
   });
 }
